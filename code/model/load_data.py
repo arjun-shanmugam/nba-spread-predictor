9fcaded3c79c2aa8e2e70e1e27637b90445c9948
AndyDelworth <andrew_delworth@brown.edu>
2021-12-04 12:59:02 -0500

    add preprocessing function to turn data file to numpy array

--- code/model/load_data.py
+++ code/model/load_data.py
@@ -1,6 +1,11 @@
+import pyreadstat
 
-def main():
-    print("hello")
 
-if __name__ == '__main__':
-    main()
+def load_data_as_numpy():
+    dtafile = '../../cleaned_data/games_and_players.dta'
+    df, meta = pyreadstat.read_dta(dtafile)
+    df.replace("", float('nan'), inplace=True) #replace empty string with NaN
+    df.dropna(axis=0, inplace=True) #remove any row w NaN
+    # print(df.head())
+    df.to_csv("../../csv_data.csv") #so that I can view the data
+    return df.to_numpy()
