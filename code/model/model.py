3f051dfb3000a50352f7a4e2ed5c420b075c731d
AndyDelworth <andrew_delworth@brown.edu>
2021-12-07 14:14:12 -0500

    Changes to Embedding model

--- code/model/model.py
+++ code/model/model.py
@@ -7,6 +7,7 @@ from preprocess import load_data_as_numpy, split_test_and_train
 class FFModelWithEmbeddings(tf.keras.Model):
     def __init__(self, batch_size):
         super(Model, self).__init__()
+        self.batch_size = batch_size
         self.E = tf.keras.layers.Embedding(batch_size, 32)
         self.normalizer = tf.keras.layers.Normalization(axis=-1)
         self.model = tf.keras.layers.Sequential([
@@ -68,10 +69,11 @@ def train(model, train_inputs, train_labels):
 
     #batch inputs and labels
 
-    while index * model.batch_size * model.window_size < len(train_inputs):
+    while index * model.batch_size < len(train_inputs):
         with tf.GradientTape() as tape:
-            loss = 1
-        
+            loss = model.loss(train_inputs[index*model.batch_size:min(index*model.batch_size+model.batch_size)],
+                              train_labels[index*model.batch_size:min(index*model.batch_size+model.batch_size)])
+            index += 1
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     return losses
@@ -89,21 +91,19 @@ def test(model, test_inputs, test_labels):
     :param model: the trained model to use for prediction
     :param test_inputs: train inputs (all inputs for testing) of shape (num_inputs,)
     :param test_labels: train labels (all labels for testing) of shape (num_labels,)
-    :returns: perplexity of the test set
+    :returns: avg loss
     """
     
-    #TODO: Fill in
-    #NOTE: Ensure a correct perplexity formula (different from raw loss)
-    summation = 0
-    losses = []
-    iterCount = 0
+    total_loss = 0
+    index = 0
 
     #batch inputs
 
-    while iterCount * model.batch_size < test_inputs.shape[0]:
-        pass
-
-    return tf.math.exp(summation / iterCount).numpy()
+    while index * model.batch_size < len(test_inputs):
+        total_loss += model.loss(test_inputs[index*model.batch_size:min(index*model.batch_size+model.batch_size)],
+                              test_labels[index*model.batch_size:min(index*model.batch_size+model.batch_size)])
+        index += 1
+    return total_loss / len(test_inputs)
 
 
 def main():
@@ -114,7 +114,8 @@ def main():
     #train for 50 epochs
     num_epochs = 5
     batch_size = 50
-    model = FFModel()
+    ff_model = FFModel()
+    ff_model_with_embeddings = FFModelWithEmbeddings(batch_size)
     #have to find a way to split into data and labels
 
 
